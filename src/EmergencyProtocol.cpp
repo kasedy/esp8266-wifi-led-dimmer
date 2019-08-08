a586a4d0ee0c3a8fe2e5c27615235eebfc77e2f8
kasedy <kasedy@gmail.com>
2019-08-08 00:47:41 -0700

    Tested STA mode of Emergency protocol

--- src/EmergencyProtocol.cpp
+++ src/EmergencyProtocol.cpp
@@ -1,50 +1,50 @@
 #include "EmergencyProtocol.h"
 #include "Ota.h"
-#include "dbg.h"
+#include "hardware.h"
 
 #include <ResetDetector.h>
 #include <ESP8266WiFi.h>
 
+#define ALLOWED_TIME_INTERVAL_BETWEEN_RESETS 3000
+#define EMERGENCY_THRESHOLD 3
+
 namespace EmergencyProtocol {
-  
-  uint8_t waitForConnectResult(unsigned long connectTimeoutMs);
-  
-  void checkOnActivation() {
-    uint8_t resetCount = ResetDetector::execute(2000);
-    DBG("Starting with number of resets = %d\n", resetCount);
-    if (resetCount < 10) {
-      return;
-    }
-    DBG("Activating emergency protocol\n");
-    digitalWrite(2, HIGH);
-    DBG("Connection to WiFi\n");
-    if (WiFi.begin() == WL_CONNECT_FAILED || waitForConnectResult(30000) != WL_CONNECTED) {
-      DBG("Connection failed. Open AP.\n");
-      WiFi.mode(WIFI_AP);
-      WiFi.softAP(DEVICE_NAME);
-    }
-    DBG("Setup Ota\n");
-    Ota::setup();
-    while (true) {
-      Ota::loop();
-      yield();
-    }
-  }
-
   uint8_t waitForConnectResult(unsigned long connectTimeoutMs) {
-    if (connectTimeoutMs == 0) {
-      return WiFi.waitForConnectResult();
-    }
-
     unsigned long start = millis();
     while (true) {
       uint8_t status = WiFi.status();
-      if (millis() > start + connectTimeoutMs 
+      bool timeout = connectTimeoutMs != 0 && millis() > start + connectTimeoutMs;
+      if (timeout
           || status == WL_CONNECTED 
           || status == WL_CONNECT_FAILED) {
         return status;
       }
       delay(100);
+    }
+  }
+  
+  void checkOnActivation() {
+    if (ResetDetector::execute(ALLOWED_TIME_INTERVAL_BETWEEN_RESETS) < EMERGENCY_THRESHOLD) {
+      return;
+    }
+    digitalWrite(2, LOW);
+    WiFi.mode(WIFI_STA);
+    waitForConnectResult(5000);
+    if (!WiFi.isConnected() && WiFi.SSID()) {
+      ETS_UART_INTR_DISABLE();
+      wifi_station_disconnect();
+      ETS_UART_INTR_ENABLE();
+      WiFi.begin();
+      waitForConnectResult(30000);
+    }
+    if (!WiFi.isConnected()) {
+      WiFi.mode(WIFI_AP);
+      WiFi.softAP(DEVICE_NAME);
+    }
+    Ota::setup();
+    while (true) {
+      Ota::loop();
+      yield();
     }
   }
 }
